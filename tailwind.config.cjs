948106975c13088444cbc695ae39843188f855bf
hex4 <121138331+Hex-4@users.noreply.github.com>
2023-12-10 10:41:45 -0700

    feat(everything): Init with svelte-add
    Created site with the Svelte-add tool. Added TW, postcss, daisy, and also added DbD colors.

---
+++ tailwind.config.cjs
@@ -0,0 +1,41 @@
+const daisyui = require("daisyui");
+const typography = require("@tailwindcss/typography");
+const forms = require("@tailwindcss/forms");
+
+/** @type {import('tailwindcss').Config}*/
+const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ffb300",
+
+          secondary: "#008fff",
+
+          accent: "#3300ff",
+
+          neutral: "#212656",
+
+          "base-100": "#0B0D1E",
+
+          info: "#00baff",
+
+          success: "#5ed000",
+
+          warning: "#ff4e00",
+
+          error: "#cc002f",
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+
+  plugins: [forms, typography, daisyui],
+};
+
+module.exports = config;
